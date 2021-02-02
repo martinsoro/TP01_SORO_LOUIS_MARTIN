6b014ef4147fe1ee007328da5cd83a0646a01fec
martinsoro <77879815+martinsoro@users.noreply.github.com>
2021-02-02 00:28:39 +0000

    mise a jour partie 2

--- src/java/com/mbds/abidjan/tp01/TestHttpServlet.java
+++ src/java/com/mbds/abidjan/tp01/TestHttpServlet.java
@@ -7,6 +7,8 @@ package com.mbds.abidjan.tp01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -69,20 +71,20 @@ public class TestHttpServlet extends HttpServlet {
             out.println("<h1 align='center'>Ma première Servlet</h1>");
             out.println("<h3>Informations concernant l'adresse IP et le navigateur utilisé par le client :</h3>");
             out.println("<p>Adressee IP du client : "  +request.getRemoteAddr()+"</p>");
-            out.println("<p>Navigateur du client : "  +request.getRemoteAddr()+"</p>");
+            out.println("<p>Navigateur du client : "  +request.getHeader("user-agent")+"</p>");
             out.println("<h3>Informations concernant la requête du client :</h3>");
-            out.println("<p>Méthode : "  +request.getRemoteAddr()+"</p>");
+            out.println("<p>Méthode : "  +request.getMethod()+"</p>");
             out.println("<p>Protocole : "  +request.getProtocol()+"</p>");
             out.println("<p>URI demandée : "  +request.getRequestURI()+"</p>");  
             out.println("<h3>Informations concernant l'entête de la requête</h3>");
             out.println("<p>Host : "  +request.getServerName()+":"+ request.getServerPort()+"</p>");
-            out.println("<p>User-agent : "  +request.getRemoteAddr()+"</p>");
-            out.println("<p>Accept : "  +request.getRemoteAddr()+"</p>"); 
-            out.println("<p>Accept-language : "  +request.getRemoteAddr()+"</p>");
-            out.println("<p>Accept-encoding : "  +request.getRemoteAddr()+"</p>");
-            out.println("<p>Accept-charset : "  +request.getRemoteAddr()+":</p>"); 
-            out.println("<p>Keep-alive : "  +request.getRemoteAddr()+"</p>");
-            out.println("<p>Connection : "  +request.getRemoteAddr()+"</p>");
+            out.println("<p>User-agent : "  +request.getHeader("user-agent")+"</p>");
+            out.println("<p>Accept : "  +request.getHeader("Accept")+"</p>"); 
+            out.println("<p>Accept-language : "  +request.getHeader("Accept-language")+"</p>");
+            out.println("<p>Accept-encoding : "  +request.getHeader("Accept-encoding")+"</p>");
+            out.println("<p>Accept-charset : "  +request.getHeader("Accept-charset")+"</p>"); 
+            out.println("<p>Keep-alive : "  +request.getHeader("keep-alive")+"</p>");
+            out.println("<p>Connection : "  +request.getHeader("connection")+"</p>");
             out.println("</body>");
             out.println("</html>");
         }
